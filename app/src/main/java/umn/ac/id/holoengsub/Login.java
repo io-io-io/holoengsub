6d9a785d19bf92c2f381d98c000cd1ae227401fe
DynamicN3 <82566683+DynamicN3@users.noreply.github.com>
2021-04-27 20:03:44 +0700

    Revert "Login & Register"
    This reverts commit 67a3affa227c6066dc586513ddb14c67a64c0c7e.

--- app/src/main/java/umn/ac/id/holoengsub/Login.java
+++ app/src/main/java/umn/ac/id/holoengsub/Login.java
@@ -6,29 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
-    EditText username, password;
-    Button signin;
-    TextView clickhere_up;
-    DBHelper DB;
-
+    public static void setOnClickListener(View.OnClickListener onClickListener) {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView clickhere_up;
+        Button signin;
 
-        username = findViewById(R.id.username1);
-        password = findViewById(R.id.password1);
-        DB = new DBHelper(this);
-
-
-        clickhere_up = this.findViewById(R.id.clickhere_up);
+        clickhere_up = (TextView) this.findViewById(R.id.clickhere_up);
         clickhere_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,34 +29,23 @@ public class Login extends AppCompatActivity {
         });
 
         //baru fungsi button, proses signin belum ada
-        signin = this.findViewById(R.id.signin);
-        signin.setOnClickListener(new View.OnClickListener() {
+        signin = (Button) this.findViewById(R.id.signin);
+        signin.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                String user = username.getText().toString();
-                String pass = password.getText().toString();
-
-                if (user.equals("") || pass.equals(""))
-                    Toast.makeText(Login.this, "Please fill all of the fields", Toast.LENGTH_SHORT).show();
-                else {
-                    Boolean checkuserpass = DB.checkusernamepassword(user, pass);
-                    if (checkuserpass) {
-                        Toast.makeText(Login.this, "Sign in successful", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(), Profile.class);
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(Login.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
-                    }
-                }
+            public void onClick(View v){
+                openProfile();
             }
-
         });
 
 
     }
 
-    public void openRegister() {
+    public void openRegister(){
         Intent intent = new Intent(this, Register.class);
         startActivity(intent);
+    }
+
+    public void openProfile(){
+        Intent intent = new Intent(this, Profile.class);
     }
 }
