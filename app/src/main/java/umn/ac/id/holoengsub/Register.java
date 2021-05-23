37ebea2646bebeb438e89a0f291c70649ecc1e3f
io-io-io <caesario@satyawiguna.com>
2021-05-23 14:14:33 +0700

    register 1.1
    little tweak, masih rusak, will update

--- app/src/main/java/umn/ac/id/holoengsub/Register.java
+++ app/src/main/java/umn/ac/id/holoengsub/Register.java
@@ -3,6 +3,7 @@ package umn.ac.id.holoengsub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,81 +17,114 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class Register extends AppCompatActivity {
-    EditText fullname, email, password, confirm_password;
-    Button signup;
-    TextView clickhere_in;
-    FirebaseAuth fAuth;
+import java.util.Objects;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+    private EditText fullname, email, password, confirm_password, phonenumber;
+    private Button signup;
+    private TextView clickhere_in;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        fullname = findViewById(R.id.fullname);
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
-        confirm_password = findViewById(R.id.confirm_password);
-        signup = findViewById(R.id.signup);
-        clickhere_in = findViewById(R.id.clickhere_in);
-        fAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        signup = (Button) findViewById(R.id.signup);
+        signup.setOnClickListener(this);
+        fullname = (EditText) findViewById(R.id.fullname);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        confirm_password = (EditText) findViewById(R.id.confirm_password);
+        phonenumber = (EditText) findViewById(R.id.phonenumber);
+        clickhere_in = (TextView) findViewById(R.id.clickhere_in);
 
-        if(fAuth.getCurrentUser() != null){
-            startActivity(new Intent(getApplicationContext(), Profile.class));
-            finish();
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.clickhere_in:
+                startActivity(new Intent(this, Login.class));
+                break;
+            case R.id.signup:
+                registeruser();
+                break;
+        }
+    }
+
+    private void registeruser() {
+        String mail = email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        String conpass = confirm_password.getText().toString().trim();
+        String name = fullname.getText().toString().trim();
+        String phone = phonenumber.getText().toString().trim();
+
+        if(name.isEmpty()){
+            fullname.setError("Please fill your name");
+            fullname.requestFocus();
+            return;
         }
 
+        if(mail.isEmpty()){
+            email.setError("Please fill your email");
+            email.requestFocus();
+            return;
+        }
 
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String mail = email.getText().toString().trim();
-                String pass = password.getText().toString().trim();
+        if(!Patterns.EMAIL_ADDRESS.matcher(mail).matches()){
+            email.setError("Please use a valid email");
+            email.requestFocus();
+            return;
+        }
 
-                //check ada yang kosong atau ga
-                if(TextUtils.isEmpty(mail)){
-                    email.setError("Please fill your email");
-                    return;
-                }
-                if(TextUtils.isEmpty(pass)){
-                    password.setError("Please fill your password");
-                    return;
-                }
-                if(pass.length() < 6){
-                    password.setError("Password must be more than 5 characters");
-                    return;
-                }
+        if(phone.isEmpty()){
+            phonenumber.setError("Please fill your phone number");
+            phonenumber.requestFocus();
+            return;
+        }
 
-                //register dan masukin data user pake firebase
-                fAuth.createUserWithEmailAndPassword(mail, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        if(pass.isEmpty()){
+            password.setError("Please fill your password");
+            password.requestFocus();
+            return;
+        }
+
+        if(!conpass.equals(pass)) {
+            confirm_password.setError("Password does not match");
+            confirm_password.requestFocus();
+        }
+
+        mAuth.createUserWithEmailAndPassword(mail, pass)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
-                            Toast.makeText(Register.this, "Sign up successful", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), Profile.class));
+                            User user = new User(name, mail, phone);
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(Register.this, "Sign up successful! ", Toast.LENGTH_LONG).show();
+                                    }else{
+                                        Toast.makeText(Register.this, "Sign up failed", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
                         }else{
-                            Toast.makeText(Register.this, "error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(Register.this, "Sign up failed", Toast.LENGTH_LONG).show();
+
                         }
                     }
                 });
 
 
-            }
-
-        });
-        clickhere_in.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), Login.class));
-            }
-        });
-
     }
-
-
-
-
-
-
 }
---
+++ app/src/main/java/umn/ac/id/holoengsub/User.java
@@ -0,0 +1,15 @@
+package umn.ac.id.holoengsub;
+
+public class User {
+    public String mail, name, phone;
+    public User(){
+
+    }
+
+    public User(String name, String mail, String phone){
+        this.name = name;
+        this.mail = mail;
+        this.phone = phone;
+    }
+
+}
